f5580b4e2e48c90bd77de7f74d7d610c179ba75c
Tymoteusz Kowalski <tymek@gft.pl>
2025-12-12 08:35:26 +0100

    Logika zczytywania z pliku

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Day 2: Rock Paper Scissors");
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/input.txt"));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Błąd odczytu pliku");
+        }
     }
 }
